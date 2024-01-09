408a652f57814a4a8d68b4beabb82080b4f136ce
ABHISHEK-RAI007 <abhishek.rai0522@gmail.com>
2024-01-09 20:06:13 +0530

    Types of data

---
+++ CH-1/1-Data-Types.js
@@ -0,0 +1,21 @@
+// console.log(document.URL);
+// console.log(document.get);
+
+// let resultedElement = document.querySelectorAll('p')
+// resultedElement = document.querySelectorAll('.coderdost')
+
+// console.log(resultedElement);
+
+let resultedElement = document.querySelector("#coders");
+console.log(resultedElement);
+
+// let dish1 = "Biryani";
+let dishes = ['Biryani', 'Chat', 'Parathey'];
+console.log(dishes[0]);
+
+dishes[0] = 'Karahi';
+console.log(dishes);
+
+console.log(dishes.join(''));
+
+console.log( dishes.indexOf("Chat") );
