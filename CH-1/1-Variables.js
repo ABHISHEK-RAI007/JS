cbd5b6623f378e492220c7ded6b809060ebf0f3e
ABHISHEK-RAI007 <abhishek.rai0522@gmail.com>
2024-01-09 20:03:18 +0530

    Variables, Constants and Comments

---
+++ CH-1/1-Variables.js
@@ -0,0 +1,19 @@
+// Variables, Constants and Comments
+
+var score = 34;
+var score = 56;
+console.log(score);
+
+
+let points = 23;
+console.log(points);
+// let points = 21;
+
+// Constants
+
+const value = 18;
+console.log(value);
+// const value = 22 
+// const value ;
+
+// 
